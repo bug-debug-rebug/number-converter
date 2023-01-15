ea6d8f821db01664c8627f6ed370c4dc03e716f0
Ivan Baykov <notarealaddress@gmail.com>
2023-01-15 14:24:55 +0700

    str2num() function was optimized.
    Test cases were redesigned.

--- num_converter.py
+++ num_converter.py
@@ -2,10 +2,23 @@
 # a module used to convert an integer number from given numeral system to desired
 # Numeral system is defined by a natural number n | 2 <= n <= 62
 
+
+def _symbol2number(c):
+    # c: char
+    # internal function used for mapping three intervals ['0'..'9'], ['a'..'z'], ['A'..'Z'] -> [0..61]
+    if c >= '0' and c <= '9':
+        return ord(c) - ord('0')
+    if c >= 'a' and c <= 'z':
+        return (ord(c) - ord('a') + 10)
+    if c >= 'A' and c <= 'Z':
+        return (ord(c) - ord('A') + 36)
+    else:
+        raise Exception("Unsupported character in a string defining converted number")
+
+
 def str2num(s, num_system):
     # s: str, num_system: int
-
-    #check the arguments
+    #check the arguments:
     if not (isinstance(s, str) and isinstance(num_system, int)):
         raise TypeError("Wrong argument type(s)")
     if num_system < 2 or num_system > 62:
@@ -13,43 +26,46 @@ def str2num(s, num_system):
     if s == '':
         raise Exception("String defining a number should have non-zero length")
     
-    n = 0
+    n = 0                               # result
+    multiplier = 1
     for i, c in enumerate(s[::-1]):
-        if c >= '0' and c <= '9':
-            converted = (ord(c) - ord('0'))
-        elif c >= 'a' and c <= 'z':
-            converted = (ord(c) - ord('a') + 10)
-        elif c >= 'A' and c <= 'Z':
-            converted = (ord(c) - ord('A') + 36)
-        else:
-            raise Exception("Unsupported character in string defining a number")
-        if converted >= num_system:
+        converted = _symbol2number(c)
+        if converted >= num_system:     # for example, if for binary system we received number 2, 3, etc instead of [0, 1]
             raise Exception("Char(s) in value-defining string does not match requested numeral system")
-        n += converted * num_system**i
+        n += converted * multiplier
+        multiplier *= num_system    # just an optimized version of: " n += converted * num_system**i "
     return n
-    
+
+
+def _number2symbol(r):
+    # r: integer
+    # internal function used for mapping interval [0..61] -> ['0'..'9'], ['a'..'z'], ['A'..'Z']
+    if r >= 0 and r <= 9:
+        return chr(r + ord('0'))
+    if r >= 10 and r <= 35:
+        return chr(r + ord('a') - 10)
+    if r >= 36 and r <= 61:
+        return chr(r + ord('A') - 36)
+    else:
+        raise Exception("Argument should be within [0..61] range")
+
+
 def num2str(a, num_system):
     # a: str, num_system: int
-
-    #check the arguments
+    # check the arguments:
     if not (isinstance(a, int) and isinstance(num_system, int)):
         raise TypeError("Wrong argument type(s)")
     if num_system < 2 or num_system > 62:
         raise Exception("Requested numeral system must be an integer wihtin [2..62] range")
     
-    res = ''
+    result = ''
     while a > 0:
-        r = a % num_system
+        remainder = a % num_system
         a = a // num_system
-        if r <= 9:
-            res = chr(r + ord('0')) + res
-        elif r >= 10 and r <= 35:
-            res = chr(r + ord('a') - 10) + res
-        elif r >= 36:
-            res = chr(r + ord('A') - 36) + res
-    return res
+        result = _number2symbol(remainder) + result
+    return result
+
 
 def convert(a, input_system, result_system):
     # a: str, input_system: int, result_system: int
     return num2str(str2num(a, input_system), result_system)
-    
--- test_convert.py
+++ test_convert.py
@@ -1,61 +1,125 @@
 # test_convert.py
 import pytest
 from num_converter import convert
+from num_converter import _symbol2number
+from num_converter import _number2symbol
 
-def test_for_repeating():
-    assert convert("123abcxyzABCXYZ", 62, 62) == "123abcxyzABCXYZ"
-
-def test_for_repeating_long():
+def test_for_self_equivalence_long(): # will not be passed on Python2 though
     assert convert("123abcxyzABCXYZ123abcxyzABCXYZ123abcxyzABCXYZ123abcxyzABCXYZ123abcxyzABCXYZ123abcxyzABCXYZ123abcxyzABCXYZ123abcxyzABCXYZ", 62, 62) == "123abcxyzABCXYZ123abcxyzABCXYZ123abcxyzABCXYZ123abcxyzABCXYZ123abcxyzABCXYZ123abcxyzABCXYZ123abcxyzABCXYZ123abcxyzABCXYZ"
 
-def test_conversion_accuracy():
-    assert convert("11111111", 2, 10) == "255"
-    assert convert("11111111", 2, 10) == "255"
-    assert convert("255", 10, 2) == "11111111"
-    assert convert("255", 10, 16) == "ff"
-    assert convert("ff", 16, 2) == "11111111"
-    assert convert("ff", 16, 10) == "255"
-    assert convert("1234567890abcdef", 16, 10) == "1311768467294899695"
-    assert convert("1311768467294899695", 10, 16) == "1234567890abcdef"
+#--------------- Boundary tests for internal _symbol2number() conversion/mapping function
 
-def test_wrong_symbols_in_value_string():
-    with pytest.raises(Exception):
-        convert(" 123", 10, 10)
-    with pytest.raises(Exception):
-        convert("123.", 10, 10)
+def test_boundary_0():  # left bounary of ['0'..'9'] range
+    assert _symbol2number('0') == 0
 
-def test_value_vs_base_mismatch():
+def test_boundary_9():  # right bounary of ['0'..'9'] range
+    assert _symbol2number('9') == 9
+
+def test_boundary_a():  # left bounary of ['a'..'z'] range
+    assert _symbol2number('a') == 10
+
+def test_boundary_z():  # right bounary of ['a'..'z'] range
+    assert _symbol2number('z') == 35
+
+def test_boundary_A():  # left bounary of ['A'..'Z'] range
+    assert _symbol2number('A') == 36
+
+def test_boundary_Z():  # right bounary of ['A'..'Z'] range
+    assert _symbol2number('Z') == 61
+
+def test_outofrange_before_0():
     with pytest.raises(Exception):
-        convert("123", 3, 10)       # 3 lies out of ternary numeral system
-        convert("1ZZ", 61, 10)      # Z lies out of 61-ary numeral syster
+        assert _symbol2number( char( ord('0') - 1) )
     
-def test_empty_value_string():
+def test_outofrange_after_9():
+    with pytest.raises(Exception):
+        assert _symbol2number( char( ord('9') + 1) )
+
+def test_outofrange_before_a():
+    with pytest.raises(Exception):
+        assert _symbol2number( char( ord('a') - 1) )
+    
+def test_outofrange_after_z():
+    with pytest.raises(Exception):
+        assert _symbol2number( char( ord('z') + 1) )
+
+def test_outofrange_before_A():
+    with pytest.raises(Exception):
+        assert _symbol2number( char( ord('A') - 1) )
+
+def test_outofrange_after_Z():
+    with pytest.raises(Exception):
+        assert _symbol2number( char( ord('Z') + 1) )
+
+
+#--------------- Boundary tests for internal _number2symbol() conversion/mapping function
+
+def test_num2sym_boundary_0():
+    assert _number2symbol(0) == '0'
+    
+def test_num2sym_boundary_9():
+    assert _number2symbol(9) == '9'
+    
+def test_num2sym_boundary_10():
+    assert _number2symbol(10) == 'a'
+    
+def test_num2sym_boundary_35():
+    assert _number2symbol(35) == 'z'
+    
+def test_num2sym_boundary_36():
+    assert _number2symbol(36) == 'A'
+    
+def test_num2sym_boundary_61():
+    assert _number2symbol(61) == 'Z'
+    
+def test_num2sys_outofrange_before_0():
+    with pytest.raises(Exception):
+        assert _number2symbol(-1)
+
+def test_num2sys_outofrange_after_61():
+    with pytest.raises(Exception):
+        assert _number2symbol(62)
+        
+    
+# ---- argument values filtering
+
+def test_empty_string():      
     with pytest.raises(Exception):
         convert("", 10, 10)
         
-def test_arguments_type():
+def test_wrong_arg1_type_notaString():
     with pytest.raises(Exception):
-        convert(12, 10, 10)
-    with pytest.raises(Exception):
-        convert(10, "12", 10)
-    with pytest.raises(Exception):
-        convert(10, 10, "12")
+        convert(10, 62, 62)
 
-def test_base_lower_than_range():
+def test_wrong_arg2_type_notAnInt():
+    with pytest.raises(Exception):
+        convert("abc", "abc", 62)
+
+def test_wrong_arg3_type_notAnInt():
+    with pytest.raises(Exception):
+        convert("abc", 62, "abc")
+
+def test_input_base_lower_than_min():
     with pytest.raises(Exception):
         convert("123", 1, 10)
+
+def test_output_base_lower_than_min():
     with pytest.raises(Exception):
         convert("123", 10, 1)
 
-def test_base_higher_than_range():
+def test_input_base_higher_than_max():
     with pytest.raises(Exception):
         convert("123", 63, 10)
+
+def test_output_base_higher_than_max():
     with pytest.raises(Exception):
         convert("123", 10, 63)
 
-def test_minmax_values():
-    assert convert("123", 62, 62) == "123"
-    assert convert("101010", 2, 2) == "101010"
+def test_numsystem_base_mismatch(): # a string containing symbols that are not allowed for exact numeral system will raise an exception
+    with pytest.raises(Exception):
+        convert("123", 3, 10)       # Symbol '3' doesn't belong to allowed symbols for ternary numeral system
 
-def test_preceding_zeroes():
+def test_preceding_zeroes():        # proves insensitivity to preceding '0' signs
     assert convert("00000123", 10, 10) == "123"
+
+
